a4bd22cad26d48e8425330283cac3c5442cb9148
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2016-05-04 23:21:05 +0600

    First commit

---
+++ README.md
@@ -0,0 +1,40 @@
+React Audio Player
+=====================
+
+HTML5 react audio player.
+
+### Usage
+
+```
+npm install
+npm start
+open http://localhost:3000
+```
+
+### Linting
+
+This react audio player project includes React-friendly ESLint configuration.
+
+```
+npm run lint
+```
+
+### Using `0.0.0.0` as Host
+
+You may want to change the host in `server.js` and `webpack.config.js` from `localhost` to `0.0.0.0` to allow access from same WiFi network. This is not enabled by default because it is reported to cause problems on Windows. This may also be useful if you're using a VM.
+
+### Dependencies
+
+* React
+* Webpack
+* [webpack-dev-server](https://github.com/webpack/webpack-dev-server)
+* [babel-loader](https://github.com/babel/babel-loader)
+* [react-hot-loader](https://github.com/gaearon/react-hot-loader)
+
+### Resources
+
+* A big thanks to Cezar Luiz.
+* [react-hot-loader on Github](https://github.com/gaearon/react-hot-loader)
+* [Integrating JSX live reload into your workflow](http://gaearon.github.io/react-hot-loader/getstarted/)
+* [Troubleshooting guide](https://github.com/gaearon/react-hot-loader/blob/master/docs/Troubleshooting.md)
+* Ping dan_abramov on Twitter or #reactjs IRC
---
+++ src/ReactAudioPlayer.js
@@ -0,0 +1,171 @@
+import './ReactAudioPlayer.scss';
+import React, { Component, PropTypes } from 'react';
+import classnames from 'classnames';
+import shuffle from 'shuffle-array';
+
+class ReactAudioPlayer extends Component {
+    state = {
+        active: this.props.songs[0],
+        current: 0,
+        progress: 0,
+        random: false,
+        repeat: false,
+        mute: false,
+        play: this.props.autoplay || false,
+        songs: this.props.songs
+    }
+
+    componentDidMount = () => {
+        let playerElement = this.refs.player;
+        playerElement.addEventListener('timeupdate', this.updateProgress);
+        playerElement.addEventListener('ended', this.end);
+        playerElement.addEventListener('error', this.next);
+    }
+
+    componentWillUnmount = () => {
+        let playerElement = this.refs.player;
+        playerElement.removeEventListener('timeupdate', this.updateProgress);
+        playerElement.removeEventListener('ended', this.end);
+        playerElement.removeEventListener('error', this.next);
+    }
+
+    setProgress = (e) => {
+        let target = e.target.nodeName === 'SPAN' ? e.target.parentNode : e.target;
+        let width = target.clientWidth;
+        let rect = target.getBoundingClientRect();
+        let offsetX = e.clientX - rect.left;
+        let duration = this.refs.player.duration;
+        let currentTime = (duration * offsetX) / width;
+        let progress = (currentTime * 100) / duration;
+
+        this.refs.player.currentTime = currentTime;
+        this.setState({ progress: progress });
+        this.play();
+    }
+
+    updateProgress = () => {
+        let duration = this.refs.player.duration;
+        let currentTime = this.refs.player.currentTime;
+        let progress = (currentTime * 100) / duration;
+
+        this.setState({ progress: progress });
+    }
+
+    play = () => {
+        this.setState({ play: true });
+        this.refs.player.play();
+    }
+
+    pause = () => {
+        this.setState({ play: false });
+        this.refs.player.pause();
+    }
+
+    toggle = () => {
+        this.state.play ? this.pause() : this.play();
+    }
+
+    end = () => {
+        (this.state.repeat) ? this.play() : this.setState({ play: false });
+    }
+
+    next = () => {
+        var total = this.state.songs.length;
+        var current = (this.state.repeat) ? this.state.current : (this.state.current < total - 1) ? this.state.current + 1 : 0;
+        var active = this.state.songs[current];
+
+        this.setState({ current: current, active: active, progress: 0 });
+
+        this.refs.player.src = active.url;
+        this.play();
+    }
+
+    previous = () => {
+        var total = this.state.songs.length;
+        var current = (this.state.current > 0) ? this.state.current - 1 : total - 1;
+        var active = this.state.songs[current];
+
+        this.setState({ current: current, active: active, progress: 0 });
+
+        this.refs.player.src = active.url;
+        this.play();
+    }
+
+    randomize = () => {
+        var s = shuffle(this.state.songs.slice());
+
+        this.setState({ songs: (!this.state.random) ? s : this.state.songs, random: !this.state.random });
+    }
+
+    repeat = () => {
+        this.setState({ repeat: !this.state.repeat });
+    }
+
+    toggleMute = () => {
+        let mute = this.state.mute;
+
+        this.setState({ mute: !this.state.mute });
+        this.refs.player.volume = (mute) ? 1 : 0;
+    }
+
+    render () {
+
+        const { active, play, progress } = this.state;
+
+        return (
+            <div className="player-container">
+                <audio src={active.url} autoPlay={this.state.play} preload="auto" ref="player"></audio>
+
+                <div className="player-cover" style={{backgroundImage: 'url('+ active.cover +')'}}></div>
+
+                <div className="artist-info">
+                    <h2 className="artist-name">{active.artist.name}</h2>
+                    <h3 className="artist-song-name">{active.artist.song}</h3>
+                </div>
+
+                <div className="player-progress-container" onClick={this.setProgress}>
+                    <span className="player-progress-value" style={{width: progress + '%'}}></span>
+                </div>
+
+
+                <div className="player-options">
+                    <div className="player-buttons player-controls">
+                        <button onClick={this.toggle} className="player-btn big" title="Play/Pause">
+                            <i className={classnames('fa', {'fa-pause': play}, {'fa-play': !play})} />
+                        </button>
+
+                        <button onClick={this.previous} className="player-btn medium" title="Previous Song">
+                            <i className="fa fa-backward" />
+                        </button>
+
+                        <button onClick={this.next} className="player-btn medium" title="Next Song">
+                            <i className="fa fa-forward" />
+                        </button>
+                    </div>
+
+                    <div className="player-buttons">
+                        <button className="player-btn small volume" onClick={this.toggleMute} title="Mute/Unmute">
+                            <i className={classnames('fa', {'fa-volume-up': !this.state.mute}, {'fa-volume-off': this.state.mute})} />
+                        </button>
+
+                        <button className={classnames('player-btn small repeat', {'active': this.state.repeat})} onClick={this.repeat} title="Repeat">
+                            <i className="fa fa-repeat" />
+                        </button>
+
+                        <button className={classnames('player-btn small random', {'active': this.state.random })} onClick={this.randomize} title="Shuffle">
+                            <i className="fa fa-random" />
+                        </button>
+                    </div>
+
+                </div>
+            </div>
+        );
+    }
+}
+
+ReactAudioPlayer.propTypes = {
+    songs: PropTypes.array.isRequired,
+    autoplay: PropTypes.bool
+};
+
+export default ReactAudioPlayer;
